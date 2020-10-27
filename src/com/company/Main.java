aaf2ee1b3afeb1cb4d3d9b96a4ade030e1731828
Dominik Majcher <majcher.dominik@op.pl>
2020-10-27 14:12:22 +0100

    delete classes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Cipher atBashCipher = new AtBashCipher();
-
-        //Encode
-        System.out.println("Encode: "+atBashCipher.encode("Ala ma kota"));
-
-        //Decode
-        System.out.println("Decode: "+atBashCipher.decode("Zoz nz plgz"));
+        
     }
 }
