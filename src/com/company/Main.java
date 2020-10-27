89abf3756b87810c0a9b0129e83c7eac910fd642
DominikM98 <72908367+DominikM98@users.noreply.github.com>
2020-10-27 14:14:17 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Cipher atBashCipher = new AtBashCipher();
-
-        // Encode: Zoz nz plgz
-        System.out.println("Encode: " + atBashCipher.encode("Ala ma kota"));
-
-        // Decode: Ala ma kota
-        System.out.println("Decode: " + atBashCipher.decode("Zoz nz plgz"));
+       
+      
     }
 
 }
