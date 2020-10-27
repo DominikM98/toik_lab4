b8c3059a900e56a9921dd0bfaefeda610ff924fa
Dominik Majcher <majcher.dominik@op.pl>
2020-10-27 13:57:04 +0100

    implements encode

--- src/com/company/Cipher.java
+++ src/com/company/Cipher.java
@@ -1,11 +1,10 @@
 package com.company;
 
 interface Cipher {
+    /** szyfrowanie wiadomosci */
+    String encode(final String message);
 
     /** deszyfrowanie wiadomosci*/
     String decode(final String message);
-
-    /** szyfrowanie wiadomosci */
-    String encode(final String message);
 }
 
